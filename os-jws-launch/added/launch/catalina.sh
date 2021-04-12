4e75fc15c8ae7f32911bf2f3a316f6c0ab780ae6
Jean-Frederic Clere <jfclere@gmail.com>
2021-04-12 13:52:03 +0200

    Arrange prometheus agent and add it to the CATALINA_OPTS environment.

--- os-jws-launch/added/launch/catalina.sh
+++ os-jws-launch/added/launch/catalina.sh
@@ -9,7 +9,7 @@ function configure() {
 function expand_catalina_opts() {
     #XXX: we should probably deprecate CATALINA_OPTS_APPEND in favor of
     #     JAVA_OPTS_APPED, which is consistent with the rest of our images.
-    CATALINA_OPTS="$CATALINA_OPTS $CATALINA_OPTS_APPEND $(/opt/jolokia/jolokia-opts)"
+    CATALINA_OPTS="$CATALINA_OPTS $CATALINA_OPTS_APPEND $(/opt/jolokia/jolokia-opts) $(/opt/jboss/container/prometheus/jws-prometheus-opts)"
 
     CATALINA_OPTS="$(adjust_java_options ${CATALINA_OPTS})"
     
