a7e53391bc3c4d38797b3770847a30c89f490c5f
vmouriki <vmouriki@redhat.com>
2022-04-04 15:17:39 +0300

    optional to https.sh

--- os-jws-https/added/https.sh
+++ os-jws-https/added/https.sh
@@ -29,7 +29,7 @@ function configure_https() {
              SSLCertificateFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE}\" \
              SSLCertificateKeyFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE_KEY}\" \
              ${password}  \
-             SSLVerifyClient=\"optional_no_ca\" SSLProtocol=\"TLSv1.3+TLSv1.2\""
+             SSLVerifyClient=\"optional\" SSLProtocol=\"TLSv1.3+TLSv1.2\""
 
       if [ -n "$JWS_SERVER_NAME" ]; then
         https="$https server=\"${JWS_SERVER_NAME}\""
