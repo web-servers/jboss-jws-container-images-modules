7fdd45958e54937e87740c457c207d35c1f57707
vmouriki <vmouriki@redhat.com>
2022-04-04 14:39:15 +0300

    old script

--- os-jws-https/added/https.sh
+++ os-jws-https/added/https.sh
@@ -3,15 +3,6 @@
 
 source $JWS_HOME/bin/launch/logging.sh
 
-
-
-
-
-
-
-
-
-
 function prepareEnv() {
   unset JWS_HTTPS_CERTIFICATE_DIR
   unset JWS_HTTPS_CERTIFICATE
@@ -26,29 +17,28 @@ function configure() {
 
 function configure_https() {
   https="<!-- No HTTPS configuration discovered -->"
+  if [ -n "${JWS_HTTPS_CERTIFICATE_DIR}" -a -n "${JWS_HTTPS_CERTIFICATE}" -a -n "${JWS_HTTPS_CERTIFICATE_KEY}" ] ; then
+      password=""
+      if [ -n "${JWS_HTTPS_CERTIFICATE_PASSWORD}" ] ; then
+          password=" SSLPassword=\"${JWS_HTTPS_CERTIFICATE_PASSWORD}\" "
+      fi
+      https="<Connector \
+             protocol=\"org.apache.coyote.http11.Http11AprProtocol\" \
+             port=\"8443\" maxThreads=\"200\" \
+             scheme=\"https\" secure=\"true\" SSLEnabled=\"true\" \
+             SSLCertificateFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE}\" \
+             SSLCertificateKeyFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE_KEY}\" \
+             ${password}  \
+             SSLVerifyClient=\"optional_no_ca\" SSLProtocol=\"TLSv1.3+TLSv1.2\""
 
-  
-  if [ -n "${JWS_HTTPS_CACERTIFICATE}" ] ; then
-	https="\
-	<Connector port=\"8443\" protocol=\"HTTP/1.1\" \
-	maxThreads=\"150\" SSLEnabled=\"true\"> \
-	<SSLHostConfig caCertificateFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CACERTIFICATE}\" 		certificateVerification=\"required\"> \
-	<Certificate certificateFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE}\" \
-	${password} \
-	certificateKeyFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE_KEY}\"/> \
-	</SSLHostConfig> \
-	</Connector>"
-  else
-	https="\
-	<Connector port=\"8443\" protocol=\"HTTP/1.1\" \
-	maxThreads=\"150\" SSLEnabled=\"true\"> \
-	<SSLHostConfig> \
-	<Certificate certificateFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE}\" \
-	${password} \
-	certificateKeyFile=\"${JWS_HTTPS_CERTIFICATE_DIR}/${JWS_HTTPS_CERTIFICATE_KEY}\"/> \
-	</SSLHostConfig> \
-	</Connector>"
+      if [ -n "$JWS_SERVER_NAME" ]; then
+        https="$https server=\"${JWS_SERVER_NAME}\""
+      fi
+
+      https="$https />"
+
+  elif [ -n "${JWS_HTTPS_CERTIFICATE_DIR}" -o -n "${JWS_HTTPS_CERTIFICATE}" -o -n "${JWS_HTTPS_CERTIFICATE_KEY}" ] ; then
+      log_warning "Partial HTTPS configuration, the https connector WILL NOT be configured."
   fi
-  
   sed -i "s|### HTTPS_CONNECTOR ###|${https}|" $JWS_HOME/conf/server.xml
 }
