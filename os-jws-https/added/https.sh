d8767e81e4abc1d95dbc422448ff38d1307ce53f
vmouriki <102514247+vmouriki@users.noreply.github.com>
2022-04-06 12:35:56 +0300

    Update https.sh

--- os-jws-https/added/https.sh
+++ os-jws-https/added/https.sh
@@ -16,8 +16,7 @@ function configure() {
 }
 
 function configure_https() {
-https="\
-<Connector SSLEnabled="true" acceptCount="100" clientAuth="false" \
+https="<Connector SSLEnabled="true" acceptCount="100" clientAuth="false" \
     disableUploadTimeout="true" enableLookups="false" maxThreads="25" \
     port="8443" keystoreFile="${JWS_HTTPS_CERTIFICATE_DIR}/.keystore" keystorePass="vmouriki" \
     protocol="org.apache.coyote.http11.Http11NioProtocol" scheme="https" \
